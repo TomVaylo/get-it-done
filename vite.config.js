0fa16617c1c7a3be638e8d8addbab4a7827e09e0
Tom Vaylo <tomvaylo@gmail.com>
2026-08-17 15:52:19 +0200

    fix: configure PWA standalone mode

--- vite.config.js
+++ vite.config.js
@@ -5,26 +5,35 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   base: '/get-it-done/',
+
   plugins: [
     react(),
     tailwindcss(),
+
     VitePWA({
       registerType: 'autoUpdate',
+
       manifest: {
+        id: '/get-it-done/',
         name: 'Get it Done!',
         short_name: 'GetItDone',
         description: 'Gestionnaire de projets et tâches épuré',
+
+        start_url: '/get-it-done/',
+        scope: '/get-it-done/',
+        display: 'standalone',
+
         theme_color: '#ea580c',
         background_color: '#fafaf9',
-        display: 'standalone',
+
         icons: [
           {
-            src: 'https://cdn-icons-png.flaticon.com/512/906/906334.png',
+            src: '/get-it-done/pwa-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'https://cdn-icons-png.flaticon.com/512/906/906334.png',
+            src: '/get-it-done/pwa-512.png',
             sizes: '512x512',
             type: 'image/png'
           }
